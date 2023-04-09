3a69b87c3ccea2b51a0b6e8c0a19e1894fdc1161
Hans Reichgelt <hreichgelt06@gmail.com>
2023-04-09 12:22:13 -0500

    apihit returns full weather need to clean up resp

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const fetch = require('node-fetch');
-const APIKEY = require('dotenv').config()
+require('dotenv').config();
+const argv = require('yargs').argv;
+const APIKEY = `${process.env.APIKEY}`
+
+//default city = La Crosse
+// argv.c = city - to call specific city use node index.js -c tokyo
+let city = argv.c || 'la crosse';
+
+//api endoint 
+let URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKEY}`
 
 
-const URL = 'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={APIKEY}';
 
-async function logWeatherJsonData(){
-    const res = await fetch(URL);
-    const jsonData = await res.json();
-    console.log(jsonData);
+async function getWeather() {
+    let response = await fetch(URL);
+    console.log(response);
+    let data = await response.json();
+    console.log(data)
+    return data;
 }
+getWeather().then(data => console.log(data));
+
+// clean up the response 
+// let weather = JSON.parse(body);
+// let message = `It's ${weather.main.temp} degrees in ${weather.name}`;
+// console.log(message);
+
+
+
