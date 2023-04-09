fc39c24dd13ee1adf6c1d13401a7691d53739d03
Hans Reichgelt <hreichgelt06@gmail.com>
2023-04-09 10:46:12 -0500

    reset dotenv

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch');
+const APIKEY = require('dotenv').config()
 
 
-const URL = 'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}'
-fetch(URL, function (err, response, body) {
-    if(err){
-        console.log('error', error);
-    } else {
-        console.log('body', body);
-    }
-});
+const URL = 'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={APIKEY}';
+
+async function logWeatherJsonData(){
+    const res = await fetch(URL);
+    const jsonData = await res.json();
+    console.log(jsonData);
+}
