cb3de80bf1f46ec5e5f0f0650ae08eaaa0d6a859
Hans Reichgelt <hreichgelt06@gmail.com>
2023-04-15 20:23:25 -0500

    zip works

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const argv = require("yargs").argv;
 const APIKEY = `${process.env.APIKEY}`;
+console.log(argv);
+
 
 //default city = La Crosse
 // argv.c = city - to call specific city use node index.js -c tokyo
 // const location = argv.c || "winona";
-var zipcode = argv.zipcode || "80031";
+// argv.zipcode = zipcode --- to call specific zip use node index.js --xxxxx
+var zipcode = argv.z || "80031";
 
 //api endoint
 // const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKEY}`; //city
